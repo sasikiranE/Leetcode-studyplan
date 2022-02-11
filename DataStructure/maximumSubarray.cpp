a3d40ca92fdd2ee1e9e3629e4d4df5cb669184c2
sasikiran <sasikiranessamaraju@gmail.com>
2022-02-11 15:06:30 +0530

    day1 data structure done.

---
+++ DataStructure/maximumSubarray.cpp
@@ -0,0 +1,50 @@
+/*
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        // using kadane's algorithm.
+        
+        int n = nums.size();
+        
+        int best = nums[0], sum = nums[0];
+        
+        for (int i = 1; i < n; i++) {
+            // if (sum + nums[i] < nums[i]) {
+            //     sum = nums[i];
+            // }
+            // else {
+            //     sum += nums[i];
+            // }
+            sum = max(nums[i], sum + nums[i]);
+            best = max(best, sum);
+        }
+        return best;
+    }
+};
